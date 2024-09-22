7a7288d37d749004d8b96577e9927d89d56e5d3f
Divina Budhathoki <divinagrb@gmail.com>
2024-09-22 19:51:41 +1000

    comments

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-var display = document.getElementById("display").querySelector("input");
-
+var display = document.getElementById("display").querySelector("input"); // getting input tag with display id by value 
+// function to execute eval() method onclick when "=" is clicked
 function calculation(){
     var presentValue = display.value;
     var result = eval(presentValue);
